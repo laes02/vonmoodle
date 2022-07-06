579e59eac77381e66657277f8fc4ef8392fef15e
laes02 <106196453+laes02@users.noreply.github.com>
2022-07-06 14:50:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def onmessage(server,message):
 
 def main():
     try:
-        vpn = MVPN('https://aulacened.uci.cu/','obiiii','Obysoft2001@')
+        vpn = MVPN('https://aulavirtual.hlg.sld.cu/','luis02','Luis02**')
         vpn.on(onmessage)
         print('MVPN SERVER RUNING!')
         vpn.run()
